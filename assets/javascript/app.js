ec691e71370bc422c24f6b58158d547d5b3b5ca0
eternal golden bread <codylewisbrock@gmail.com>
2019-10-30 06:50:23 -0700

    adds the ability to toggle between the gif and the static img as the source

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,16 +2,58 @@ var gifsArr = ["happy", "surprised", "shocked", "interested"];
 
 var localGifStorage = {};
 
-function populateImages(array) {
+function populateImages(array, dataName) {
     console.log(array)
-    array.forEach(element => {
-        $("#gif-field").append($(`<img src="${element.static}">`))
-    });
+    for (let i = 0; i < array.length; i++) {
+        $("#gif-field").append($(`<img class="startStop static" src="${array[i].static}" data-name="${dataName}" id=${i}>`));
+    }
+    
+
+            //OLD WORKIGN VERSION
+    // array.forEach(element => {
+    //     $("#gif-field").append($(`<img class="startStop" src="${element.static}">`));
+    // });
+
 }
+
+function animateImageOnClick() {
+    console.log("regular",this);
+    console.log("jquery",$(this));
+    console.log(localGifStorage);
+    console.log($(this).attr('data-name'));
+    console.log($(this).attr('id'));
+
+    let dataName = $(this).attr('data-name');
+    console.log("dataName", dataName);
+    let dataId = $(this).attr('id');
+    console.log("dataId", dataId);
+
+
+
+    var currentSource = $(this).attr('src');
+    console.log("currentSource", currentSource);
+
+    var staticVsAnimated
+    if ($(this).attr('class').includes('static')) {
+        let newClass = 'animated'
+        staticVsAnimated = localGifStorage[dataName][dataId][newClass];
+        $(this).toggleClass('static');
+    } else {
+        let newClass = 'static'
+        staticVsAnimated = localGifStorage[dataName][dataId][newClass];
+        $(this).toggleClass('static');
+    }
+
+    console.log('staticVsAnimated', staticVsAnimated);
+    $(this).attr('src', staticVsAnimated);
+    
+}
+
 
 function displayGifs() {
     let gifQuery = $(this).text()
     let queryURL = "https://api.giphy.com/v1/gifs/search?api_key=CJRlt8fWwfRQ8V9FigI6bffN4mN5zMgt&q=" + gifQuery + "&limit=10&offset=0&rating=G&lang=en";
+    // let giphyName = $(this).text("data-name")
 
     if (localGifStorage[gifQuery]) {
         populateImages(localGifStorage[gifQuery]);
@@ -29,7 +71,7 @@ function displayGifs() {
                 elementArr.push(tempObj);
             });
             localGifStorage[gifQuery] = elementArr;
-            populateImages(localGifStorage[gifQuery]);
+            populateImages(localGifStorage[gifQuery], gifQuery);
         })
     }
     // console.log(localGifStorage)
@@ -62,3 +104,6 @@ $("#add-gif").on("click", function (event) {
 renderButtons();
 
 $(document).on("click", ".gif", displayGifs);
+
+$(document).on("click", ".startStop", animateImageOnClick);
+//build function
